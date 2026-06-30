6508a4eea779f2e415c05d49664691552eb6b22e
Toe Htet Hte Lwin <toehtethtetlwin123456@gmail.com>
2026-06-30 21:47:30 +0630

    dd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LoveComponent } from './love/love.component';
 
 export const routes: Routes = [
-  // 1. Not Found အတွက် သီးသန့် route (ဒါကို :id ရဲ့ အပေါ်မှာ ထားပါ)
+  
   { path: 'not-found', component: NotFoundComponent },
-
-  // 2. Dynamic ID (c01, c02 စသဖြင့်)
+ 
   { path: ':id', component: LoveComponent },
 
-  // 3. Link အလွတ်နဲ့ ဝင်လာရင် not-found ကိုပဲ သွားမယ်
   { path: '', redirectTo: '/not-found', pathMatch: 'full' },
 
-  // 4. တခြား ဘာရိုက်ရိုက် not-found ကို သွားမယ်
   { path: '**', redirectTo: '/not-found' }
 ];
